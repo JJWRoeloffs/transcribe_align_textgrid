b02f48528794b69601279503309f8d4a4dfc6cc5
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-03-27 22:00:20 +0200

    Reformat adapter for legibility

--- src/transcribe_allign_textgrid/adapter.py
+++ src/transcribe_allign_textgrid/adapter.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
-from typing import Dict
+from typing import Dict, List
 
 from praatio.data_classes.interval_tier import IntervalTier
 from praatio.data_classes.textgrid import Textgrid
 from praatio.utilities.constants import Interval
 
 from transcribe_allign_textgrid.whisper_schema import WHISPER_VALIDATOR
+
+
+def fill_if_empty(entries: List[Interval]) -> List[Interval]:
+    return entries if entries else [Interval(start=0.0, end=0.1, label="")]
 
 
 class WhisperWord:
@@ -50,27 +54,26 @@ class WhisperObject:
 
     def to_segment_text_tier(self) -> IntervalTier:
         entries = [x.to_text_interval() for x in self.segments]
-        entries = entries if entries else [Interval(start=0.0, end=0.1, label="")]
-        return IntervalTier("segments_text", entries)
+        return IntervalTier("segments_text", fill_if_empty(entries))
 
     def to_segment_confidence_tier(self) -> IntervalTier:
         entries = [x.to_confidence_interval() for x in self.segments]
-        entries = entries if entries else [Interval(start=0.0, end=0.1, label="")]
-        return IntervalTier("segments_confidence", entries)
+        return IntervalTier("segments_confidence", fill_if_empty(entries))
 
     def to_word_text_tier(self) -> IntervalTier:
         entries = [x.to_text_interval() for y in self.segments for x in y.words]
-        entries = entries if entries else [Interval(start=0.0, end=0.1, label="")]
-        return IntervalTier("words_text", entries)
+        return IntervalTier("words_text", fill_if_empty(entries))
 
     def to_word_confidence_tier(self) -> IntervalTier:
         entries = [x.to_confidence_interval() for y in self.segments for x in y.words]
-        entries = entries if entries else [Interval(start=0.0, end=0.1, label="")]
-        return IntervalTier("words_confidence", entries)
+        return IntervalTier("words_confidence", fill_if_empty(entries))
 
 
 def whisper_to_textgrid(whisper_timestamped_output: Dict) -> Textgrid:
-    WHISPER_VALIDATOR.validate(whisper_timestamped_output)
+    try:
+        WHISPER_VALIDATOR.validate(whisper_timestamped_output)
+    except Exception as err:
+        raise ValueError(f"Error converting Whisper-timestamped to Textgrid: {err}")
 
     whisper_object = WhisperObject(whisper_timestamped_output)
 
