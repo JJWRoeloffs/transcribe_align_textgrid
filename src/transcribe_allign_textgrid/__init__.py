2fde61113864f8f6ac50ed70ae74f99a78ad1bf4
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-11-13 22:07:11 +0100

    Bump version

--- src/transcribe_allign_textgrid/__init__.py
+++ src/transcribe_allign_textgrid/__init__.py
@@ -21,7 +21,7 @@ along with this program.  If not see LICENSE for more details.
 __title__ = "transcribe_allign_textgrid"
 __author__ = "JJWRoeloffs"
 __license__ = "AGPL-3.0"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 
 from transcribe_allign_textgrid.adapter import (
